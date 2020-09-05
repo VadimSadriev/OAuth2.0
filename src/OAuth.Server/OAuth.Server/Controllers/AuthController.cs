57af18cf2e0e118e6594b3a846921f9889b8976c
Vadim <SadrievVF@it2g.ru>
2020-09-06 00:19:02 +0500

    add register view

--- src/OAuth.Server/OAuth.Server/Controllers/AuthController.cs
+++ src/OAuth.Server/OAuth.Server/Controllers/AuthController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OAuth.Server.Contracts;
+using System.Threading.Tasks;
 
 namespace OAuth.Server.Controllers
 {
     public class AuthController : Controller
     {
+        public IActionResult Register()
+        {
+            ViewData["Title"] = "Register an account";
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody]RegisterAccountContract contract)
+        {
+            return Ok();
+        }
     }
 }
